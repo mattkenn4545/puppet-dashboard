05a00eee1037072b2d456de626d421c64e56940c
Matthew Kennedy <mkennedy@clearwateranalytics.com>
2013-05-22 11:09:05 -0600

    Use mysql module install mysql-server

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,14 +1,29 @@
-class puppet-dashboard {
+class puppet-dashboard ( $db_password ) {
   package { 'puppet-dashboard':
     ensure => 'installed',
   }
 
-  file { ['settings.yml', 'database.yml']:
-    mode    => '0660',
-    owner   => 'root',
-    group   => 'root',
-    source  => "puppet:///modules/${module_name}/${name}",
-    path    => "/etc/puppet-dashboard/${name}",
-    ensure  => Package['puppet-dashboard'],
+  class { 'mysql::server':
+    config_hash => {
+      'root_password' => $db_password
+    }
+  }
+
+  file { 'settings.yml':
+    mode    => '0640',
+    owner   => 'www-data',
+    group   => 'www-data',
+    source  => "puppet:///modules/${module_name}/settings.yml",
+    path    => "/etc/puppet-dashboard/settings.yml",
+    require => Package[ 'puppet-dashboard' ]
+  }
+
+  file { 'database.yml':
+    mode    => '0640',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => template("${module_name}/database.yml.erb"),
+    path    => "/etc/puppet-dashboard/database.yml",
+    require => Package[ 'puppet-dashboard' ]
   }
 }

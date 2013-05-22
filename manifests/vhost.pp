e6268cf9470507afbf11c6ea4df6d9f301c60b07
Matthew Kennedy <mkennedy@clearwateranalytics.com>
2013-05-22 13:50:56 -0600

    Forgot docroot

--- manifests/vhost.pp
+++ manifests/vhost.pp
@@ -1,15 +1,16 @@
 class puppet-dashboard::vhost {
 
   $dashboard_port = 3000
-  $docroot = ''
+  $docroot = '/usr/share/puppet-dashboard/public'
   $rails_base_uri = '/'
+  $server_name = 'puppet'
 
   file { 'dashboard-vhost':
     mode    => '0644',
     owner   => 'www-data',
     group   => 'www-data',
     content => template("${module_name}/puppetdashboard.erb"),
-    path    => "/etc/apache/sites-available/puppetdashboard",
+    path    => "/etc/apache2/sites-available/puppetdashboard",
     require => Package[ 'puppet-dashboard' ]
   }
 }

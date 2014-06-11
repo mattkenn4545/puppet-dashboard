938a8827b425a1c5590b4df1b74589cf28978e2c
Matthew Kennedy <mattkenn4545@gmail.com>
2014-06-10 21:16:56 -0600

    cleanup puppetdashboard vhost. Other fixes.

--- manifests/vhost.pp
+++ manifests/vhost.pp
@@ -1,22 +1,25 @@
-class puppet-dashboard::vhost {
+class puppet-dashboard::vhost (
+  $auth = false
+) {
   $dashboard_port = 3000
   $docroot = '/usr/share/puppet-dashboard/public'
   $rails_base_uri = '/'
   $server_name = 'puppet'
 
   file { 'dashboard-vhost':
-    mode    => '0644',
-    owner   => 'www-data',
-    group   => 'www-data',
-    content => template("${module_name}/puppetdashboard.erb"),
-    path    => "/etc/apache2/sites-available/puppetdashboard",
-    require => Package[ 'puppet-dashboard' ]
+    mode      => '0644',
+    owner     => 'www-data',
+    group     => 'www-data',
+    content   => template("${module_name}/puppetdashboard.erb"),
+    path      => "/etc/apache2/sites-available/puppetdashboard",
+    require   => Package[ 'puppet-dashboard' ],
+    notify    => Service['apache2']
   }
 
   exec { 'enable puppetdashboard vhost':
     command   => 'a2ensite puppetdashboard',
     creates   => '/etc/apache2/sites-enabled/puppetdashboard',
-    require   => File['dashboard-vhost'],
+    require   => File['dashboard-vhost']
   }
 
   firewall { '100 allow puppetdashboard':
